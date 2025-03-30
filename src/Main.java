b9c1db153b2e54ff156987c1c6182b56bfcaf26d
Danel <danel.li@mail.ru>
2025-03-30 19:26:28 +0500

    6th problem code and explanation

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ import java.util.Scanner; //here we import class Scanner from package java.util 
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in); //creating object sc to input nums from console
+        int a=sc.nextInt(); //input a num
         int n=sc.nextInt(); //input n num
         sc.close();//close the scanner
 
-        System.out.print(fib(n)); //send n to fib function and print the returned result
+        System.out.print(powNum(a,n)); //send n to fib function and print the returned result
     }
 
-    public static int fib(int n){
-        if(n==1) return 1; //since fib list starts with 0 1 1 2 3 5 ..., for first two elements there is no pattern, so we create a specific conditions for them
-        if(n==0) return 0;
+    public static int powNum(int a, int n){
+        if (n==0) return 1;
+        if(n==1) return a;
 
-
-        return fib(n-1)+fib(n-2); //as said in pr5, the nth num in fib list equals to fun(n-1)+fib(n-2)
+        return a*powNum(a, n-1);
 
 
     }
